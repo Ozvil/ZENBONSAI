3b4afa538d374b7df6caf93af6769eb4b26fdcbe
Ozvil <javiervillavicencio1@gmail.com>
2025-08-14 17:00:35 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,4 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-function registerSW() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').catch(console.error)
-    })
-  }
-}
-registerSW()
-
 createRoot(document.getElementById('root')).render(<App />)
